c92d74792ee0d1fdc3ae9c20b4660e277bb118fd
ali <alineamati.1381@gmail.com>
2022-09-25 20:25:37 +0330

    navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary" dir="rtl">
+    <div class="container-fluid">
+      <a class="navbar-brand link-light" href="#">وبلاگ</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbar"
+        aria-controls="navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex justify-content-between" id="navbar">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link link-light active" aria-current="page" href="#">خانه</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="#">درباره ما</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="#">ارتباط با ما</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle link-light"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              مقاله ها
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item text-center" href="#">علمی</a></li>
+              <li>
+                <a class="dropdown-item text-center" href="#">برنامه نویسی</a>
+              </li>
+              <li>
+                <a class="dropdown-item text-center" href="#">محیط زیست</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <form class="me-4">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="جستجو"
+            aria-label="Search"
+          />
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
