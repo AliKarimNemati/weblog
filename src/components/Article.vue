a2a3eab18b1f068154e39272afb465a1b1f6a268
ali <alineamati.1381@gmail.com>
2022-09-27 13:21:49 +0330

    keyword option added

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="card mb-3 p-0 container-fluid col-10 article" dir="rtl">
+  <div class="card mb-3 p-0 container-fluid col-10 article" dir="rtl" v-if="isKeyword[article.id - 1]">
     <div class="row g-0">
       <div class="col-md-6">
-        <img
-          :src="article.imgSrc"
-          class="img-fluid w-100 img-article"
-        />
+        <img :src="article.imgSrc" class="img-fluid w-100 img-article" />
       </div>
       <div class="col-md-6">
         <div class="justify-content-between d-none d-md-flex">
           <div class="d-flex">
-            <a href="#"  v-for="(keyword, i) in article.keywords" :key="i" class="text-muted bg-keyword rounded-pill p-2 m-2 text-decoration-none "> {{keyword}} </a>
+            <button
+              @click="handleKeyword"
+              v-for="(keyword, i) in article.keywords"
+              :key="i"
+              class="text-muted bg-keyword rounded-pill p-2 m-2 btn"
+              :value="keyword"
+            >
+              {{ keyword }}
+            </button>
           </div>
           <p class="card-text align-self-center m-3">
             <small class="text-muted">{{ article.date }}</small>
           </p>
         </div>
         <div class="card-body">
-          <h5 class="card-title"> {{article.title}} </h5>
-          <p class="card-text"> {{article.shortContent}} </p>
+          <h5 class="card-title">{{ article.title }}</h5>
+          <p class="card-text">{{ article.shortContent }}</p>
           <nav class="text-center h5">
-            <a class="text-decoration-none btn btn-primary link-light mt-3" href="./article.html"
+            <a class="text-decoration-none btn btn-primary mt-3" href=""
               >ادامه مقاله</a
             >
           </nav>
@@ -32,12 +37,18 @@
 
 <script>
 export default {
-  props: ['article'],
+  props: ["article", "isKeyword"],
+  methods: {
+    handleKeyword(e) {
+      this.$emit('setKeyword', e.target.value)
+      console.log(this.isKeyword);
+    },
+  },
 };
 </script>
 
 <style>
-.img-article{
-  border-radius:0 5px 5px 0;
+.img-article {
+  border-radius: 0 5px 5px 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <Navbar />
-  <Article class="mt-5" v-for="(article , i) in articles" :key="i" :article="article"/>
+  <Article
+    class="mt-5"
+    v-for="(article, i) in articles"
+    :key="i"
+    :article="article"
+    :isKeyword="isKeyword"
+    @setKeyword="setKeyword"
+  />
 </template>
 
 <script>
@@ -9,13 +16,30 @@ import Article from "../components/Article.vue";
 export default {
   name: "Home",
   components: { Navbar, Article },
-  data(){
-    return{
-      articles:[]
-    }
+  data() {
+    return {
+      articles: [],
+      keyword: "",
+      isKeyword: [true, true],
+    };
   },
-  async mounted(){
-    this.articles = (await (await fetch('data/db.json')).json()).articles;
-  }
+
+  methods: {
+    setKeyword(e) {
+      this.articles.forEach((article, index) => {
+        for (let i = 0; i < article.keywords.length; i++) {
+          if (article.keywords[i] == e) {
+            this.isKeyword[index] = true;
+            break;
+          } else {
+            this.isKeyword[index] = false;
+          }
+        }
+      });
+    },
+  },
+  async mounted() {
+    this.articles = (await (await fetch("data/db.json")).json()).articles;
+  },
 };
 </script>
